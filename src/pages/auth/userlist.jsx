16f5ade44c7cab393b1f095bbdc2db48f3ebea28
Efe-Roy <dakaraefe@gmail.com>
2024-01-09 09:28:42 +0100

    name changed

--- src/pages/auth/userlist.jsx
+++ src/pages/auth/userlist.jsx
@@ -331,7 +331,7 @@ const UserList = () => {
 
         </div>
 
-        <h1 className="sm:text-lg text-sm font-bold title-font text-center text-white mt-10">ARCHIVO NO ASIGNADO</h1>
+        <h1 className="sm:text-lg text-sm font-bold title-font text-center text-white mt-10">LISTA DE USUARIOS CREADA POR USTED</h1>
         <div className="flex flex-col p-3">
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
